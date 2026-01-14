13a5e7b6511bd6037f4021112f9d9b5a2d5d0035
kendriks <kendriksdapaixao@gmail.com>
2026-01-13 22:42:55 -0300

    refactor: DOM - app.component.spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,7 +26,7 @@ describe('AppComponent', () => {
   it('should render title in a h1 tag', waitForAsync(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
+    const compiled = fixture.debugElement;
     expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!!');
   }));
 });
